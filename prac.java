9f23f4d51e81b616d2b998f957558df018134691
Davinder Ratwal <105744095+Dave8289@users.noreply.github.com>
2023-01-06 12:58:12 +0530

    Update prac.java

--- prac.java
+++ prac.java
@@ -1,36 +1,22 @@
-import Graphs.GraphUsingMap;
+import java.util.ArrayList;
 
 public class prac {
     public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        int start = 0;
+        int end = list.size() - 1;
+        System.out.print(list);
+        while (start >= end) {
+            if (list.get(start) != list.get(end)) {
+                System.out.println("false");
+            } else {
+                start++;
+                end--;
+            }
+        }
+        System.out.println("true");
 
-        GraphUsingMap g = new GraphUsingMap();
-        // g.addVertex("A");
-        // g.addVertex("B");
-        // g.addVertex("C");
-        // g.addVertex("D");
-        // g.addVertex("E");
-        // g.addVertex("F");
-
-        // g.addEdge("A", "B", 1);
-        // g.addEdge("A", "D", 1);
-        // g.addEdge("B", "C", 1);
-        // g.addEdge("C", "D", 1);
-        // g.addEdge("D", "E", 1);
-        // g.addEdge("E", "F", 1);
-        // g.addEdge("B", "D", 1);
-        // g.removeEdge("B", "D");
-        // g.removeEdge("D", "E");
-
-        // System.out.println(g.isConnected());
-        g.addVertex("A");
-        g.addVertex("B");
-        g.addVertex("C");
-        g.addVertex("D");
-        g.addVertex("E");
-        
-
-        g.addEdge("A", "B", 2);
-        g.addEdge("C", "D", 1);
-        System.out.println(g.getCC());
     }
 }
